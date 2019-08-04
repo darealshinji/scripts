e4878d414ba9d37c5282dcc205101852237f5863
darealshinji <djcj@gmx.de>
2019-08-04 23:48:08 +0200

    Update install-vs.sh

--- Hybrid/install-vs.sh
+++ Hybrid/install-vs.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+counter ()
+{
+  printf "$2"
+  while kill -0 $1 2>/dev/null ; do
+    printf "."
+    sleep 0.5
+  done
+  printf "\n"
+}
+
 vsprefix="$HOME/opt/vapoursynth"
 
 if ! pkg-config --exists python3 ; then
@@ -13,32 +23,32 @@ if [ -e "$vsprefix" ]; then
 fi
 
 set -e
-#set -x
 
 cd "$(dirname "$0")"
 
-echo "Copy files."
+# copy
 sitepackages="$vsprefix/lib/python3/site-packages"
 mkdir -p "$sitepackages"
-cp -r include lib vsplugins "$vsprefix"
+cp -r include lib vsplugins "$vsprefix" &
+counter $! "Copy files"
 
-echo "Build Python module."
+# cython
 gcc -shared -fPIC -O3 -Isrc  $(pkg-config --cflags python3) \
   src/src/cython/vapoursynth.c  -o "$sitepackages/vapoursynth.so" \
-  -Llib -lvapoursynth  -s -Wl,-rpath,"$vsprefix/lib"
+  -Llib -lvapoursynth  -s -Wl,-rpath,"$vsprefix/lib" &
+counter $! "Build Python module"
 
-echo "Build VSScript library."
+# vsscript
 g++ -shared -fPIC -O3 -Isrc/include  $(pkg-config --cflags python3) \
   src/src/vsscript/vsscript.cpp  -o "$vsprefix/lib/libvapoursynth-script.so.0" \
-  $(pkg-config --libs python3)  -s -Wl,-rpath,"$vsprefix/lib"  -Wl,-soname,libvapoursynth-script.so.0
-
+  $(pkg-config --libs python3)  -s -Wl,-rpath,"$vsprefix/lib"  -Wl,-soname,libvapoursynth-script.so.0 &
+counter $! "Build VSScript library"
 ln -sf libvapoursynth-script.so.0 "$vsprefix/lib/libvapoursynth-script.so"
 
-echo "Build VSPipe."
+# vspipe
 g++ -O3 -Isrc/include  src/src/vspipe/vspipe.cpp  -o "$vsprefix/vspipe" \
-  -L"$vsprefix/lib" -lvapoursynth-script  -s -Wl,-rpath,"$vsprefix/lib"
-
-#set +x
+  -L"$vsprefix/lib" -lvapoursynth-script  -s -Wl,-rpath,"$vsprefix/lib" &
+counter $! "Build VSPipe"
 
 echo "Create \`$vsprefix/env.sh'"
 cat <<EOF >"$vsprefix/env.sh"
