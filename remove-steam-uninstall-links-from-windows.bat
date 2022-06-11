9d36d2130b18291fde7b389d6e12338b15b2f46f
darealshinji <djcj@gmx.de>
2022-06-11 03:14:29 +0200

    Update remove-steam-uninstall-links-from-windows.bat
    Show the ID and names of the games

--- remove-steam-uninstall-links-from-windows.bat
+++ remove-steam-uninstall-links-from-windows.bat
@@ -1,17 +1,22 @@
 @echo off
-
-REM http://furgelnod.com/2014/removing-steam-games-from-programs-and-features-windows/
+rem http://furgelnod.com/2014/removing-steam-games-from-programs-and-features-windows/
 
 @title Remove Steam Uninstall Links From Windows
 @net session >nul 2>&1
 @if %errorlevel% == 0 (
-    @for /F "delims=" %%a in ('reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall ^| findstr /C:"Steam App"') do @reg delete "%%a" /f
-    @for /F "delims=" %%a in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall ^| findstr /C:"Steam App"') do @reg delete "%%a" /f
+    @for /F "delims=" %%a in ('reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall ^| findstr /C:"Steam App "') do (
+        @reg query "%%a" /v "DisplayName"
+        @reg delete "%%a" /f
+        @echo.
+    )
+    @for /F "delims=" %%a in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall ^| findstr /C:"Steam App "') do (
+        @reg query "%%a" /v "DisplayName"
+        @reg delete "%%a" /f
+        @echo.
+    )
     @echo Success!
-    @echo.
-    @pause
 ) else (
     @echo You do not have Administrator Priveleges. Try right clicking and choosing 'run as administrator '
-    @echo.
-    @pause
 )
+@echo.
+@pause
